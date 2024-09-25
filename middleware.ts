f6be0d2dca4516c3f83ee239ec959646fdeec023
“Techanzy <techanzy@gmail.com>
2024-09-25 13:22:14 +0500

    udpates

--- middleware.ts
+++ middleware.ts
@@ -10,9 +10,15 @@ export async function middleware(req: NextRequest) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  if (!session) {
-    return NextResponse.rewrite(new URL("/login", req.url));
+  const { pathname } = req.nextUrl;
+
+  // If no session exists, and the user is not already on the /login page, redirect to /login
+  if (!session && pathname !== "/login") {
+    const loginUrl = new URL("/login", req.url);
+    return NextResponse.redirect(loginUrl);
   }
+
+  return res; // If session exists or already on /login, continue
 }
 
 export const config = {
