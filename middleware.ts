380c535f91169cfab0c20738c56c19e2d884868f
“Techanzy <techanzy@gmail.com>
2024-09-30 02:04:48 +0500

    updates

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ export async function middleware(req: NextRequest) {
   const {
     data: { session },
   } = await supabase.auth.getSession();
+  console.log("sessions", session)
 
   const { pathname } = req.nextUrl;
 
