f11c3646369d07a91c1dd9ddb5efe5d40032b207
“Techanzy <techanzy@gmail.com>
2024-09-30 02:07:27 +0500

    updates

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-
   const supabase = createMiddlewareClient({ req, res });
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
-  console.log("sessions", session)
+
+  console.log("Session:", session); // Log session information
 
   const { pathname } = req.nextUrl;
 
